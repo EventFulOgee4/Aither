d9202d3838eb8f3d3b8730691c5863bfb527db5e
bahmed721 <s.bilal.ahmed927@gmail.com>
2026-01-17 15:03:52 -0700

    Started emotion.py

--- model/aither/modules/emotion.py
+++ model/aither/modules/emotion.py
@@ -1,1 +1,9 @@
-#Emotional Tones of Aither
+#Emotional Tones of Aither : Assertive, Tender, Empathethic, and Neutral
+
+class AitherEmotionalTones():   
+    EMOTIONAL_TONES = {
+        "ASSERTIVE" : "",
+        "TENDER" : "",
+        "EMPATHETHIC" : "",
+        "NEUTRAL/DEFAULT" : ""
+    }
