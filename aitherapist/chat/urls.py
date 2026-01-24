45ce6ebb21faa85e5217a1164ffa77518b1202f2
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-01-23 23:27:01 -0700

    Created test endpoint

--- aitherapist/chat/urls.py
+++ aitherapist/chat/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import test_api
 from .views import TherapySessionViewSet, ChatMessageViewSet
 
 urlpatterns = [
     path('sessions/', TherapySessionViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('messages/', ChatMessageViewSet.as_view({'get': 'list', 'post': 'create'})),
+]
+
+urlpatterns = [
+    path('test/', test_api),
 ]
