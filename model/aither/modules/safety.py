9d525ecbb4dd0ec3589f30fb5518c5bc7f2766f5
bahmed721 <s.bilal.ahmed927@gmail.com>
2026-01-24 16:35:26 -0700

    Finished crisis patterns logic

--- model/aither/modules/safety.py
+++ model/aither/modules/safety.py
@@ -24,26 +24,49 @@ class AitherSafety():
     def __init__(self, memory):
         self.memory = memory
         self.crisis_patterns = {
-            "explicitness" : {
-                "patterns" : ["end it all", "better off", "going to do it", "not needed", "unalive"], 
-                "severity" : RiskAssessment.CRITICAL
-            },
-            "hopelessness" : {
-                "patterns" : ["no one likes me", "I'm hated"],
-                "severity" : RiskAssessment.URGENT
-            },
-            "unwell" : {
-                "patterns" : [],
-                "severity" : RiskAssessment.CONCERNED
-            },
-            "distress" : {
-                "patterns" : [],
-                "severity" : RiskAssessment.WATCHFUL
-            },
-            "healthy" : {
-                "patterns" : [],
-                "severity" : RiskAssessment.BASELINE
-            }
+        "explicitness": {
+            "patterns": [
+                "kill myself", "end my life", "want to die", "don't want to be here",
+                "end it all", "better off dead", "going to do it", "not needed",
+                "unalive", "no reason to live", "want it to be over", "can't do this anymore",
+                "final goodbye", "won't be around", "checking out", "take my own life",
+                "not gonna be here", "plan to end", "suicide", "hurt myself"
+            ],
+            "severity": RiskAssessment.CRITICAL
+        },
+        "hopelessness": {
+            "patterns": [
+                "no one cares", "no one likes me", "I'm hated", "burden to everyone",
+                "they'd be better without me", "no point anymore", "given up",
+                "no future", "no way out", "nothing will change", "trapped forever",
+                "no one would notice", "completely alone", "nobody loves me",
+                "worthless", "waste of space", "shouldn't exist", "don't belong"
+            ],
+            "severity": RiskAssessment.URGENT
+        },
+        "unwell": {
+            "patterns": [
+                "can't get out of bed", "stopped eating", "can't sleep", "sleep all day",
+                "hate myself", "I'm a failure", "nothing matters", "don't care anymore",
+                "crying all the time", "empty inside", "numb", "lost interest",
+                "can't function", "falling apart", "barely surviving", "going through the motions",
+                "isolating myself", "pushing everyone away", "self harm", "cutting"
+            ],
+            "severity": RiskAssessment.CONCERNED
+        },
+        "distress": {
+            "patterns": [
+                "overwhelmed", "can't cope", "stressed out", "anxious", "panic",
+                "breaking down", "losing it", "can't handle", "so tired of",
+                "exhausted", "burned out", "frustrated", "lonely", "scared",
+                "worried sick", "can't relax", "on edge", "feeling down", "sad lately"
+            ],
+            "severity": RiskAssessment.WATCHFUL
+        },
+        "healthy": {
+            "patterns": [],  # Default state - absence of concerning patterns
+            "severity": RiskAssessment.BASELINE
         }
+    }
         
 
