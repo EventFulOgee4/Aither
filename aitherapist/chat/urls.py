dcb2873dfe3987463addb96f720e7c401303a216
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-01-24 17:14:35 -0700

    Updated to just one urlpatterns

--- aitherapist/chat/urls.py
+++ aitherapist/chat/urls.py
@@ -3,10 +3,8 @@ from .views import test_api
 from .views import TherapySessionViewSet, ChatMessageViewSet
 
 urlpatterns = [
+    path('test/', test_api),
     path('sessions/', TherapySessionViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('messages/', ChatMessageViewSet.as_view({'get': 'list', 'post': 'create'})),
 ]
 
-urlpatterns = [
-    path('test/', test_api),
-]
