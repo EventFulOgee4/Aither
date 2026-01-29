6fafb71b5ea7255235ca620d3f7ec0fb77188125
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-01-29 10:52:43 -0700

    deleted chat_messages, worked on login and registration

--- aitherapist/chat_messages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ChatMessageViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'', ChatMessageViewSet, basename='messages')
-
-urlpatterns = router.urls
--- aitherapist/users/views.py
+++ aitherapist/users/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, decorators, response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-    permission_classes = []
+    permission_classes = [permissions.AllowAny]
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    user = request.user
+    return Response({
+        "username": user.username,
+        "email": user.email
+    })
