fecd6cb8e91597fbbd57cc4c7a19b26348f979d7
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-01-29 16:24:00 -0700

    Tested login, regester, auth tokens, and refresh in Thunder Client. Also saw a coded ai response.

--- aitherapist/chat/views.py
+++ aitherapist/chat/views.py
@@ -29,21 +29,27 @@ class ChatMessageViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return ChatMessage.objects.filter(session__user=self.request.user)
+        queryset = ChatMessage.objects.filter(
+            session__user=self.request.user
+        )
+
+        session_id = self.request.query_params.get("session")
+        if session_id:
+            queryset = queryset.filter(session_id=session_id)
+
+        return queryset
 
     def perform_create(self, serializer):
-        # 1. Save user message
         user_message = serializer.save(sender="user")
 
-        # 2. Generate AI response
         ai_text = get_ai_response(user_message.message)
 
-        # 3. Save AI message
         ChatMessage.objects.create(
             session=user_message.session,
             sender="ai",
             message=ai_text
         )
+
         
 
 @api_view(['GET'])
