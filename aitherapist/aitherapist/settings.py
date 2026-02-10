5c02decd4741a058c17a6afbdd2037fc96b75dfe
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-02-09 19:20:01 -0700

    Worked on pagination and database indexing

--- aitherapist/aitherapist/settings.py
+++ aitherapist/aitherapist/settings.py
@@ -123,7 +123,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    #Added for pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE": 20,
 }
 
 SIMPLE_JWT = {
