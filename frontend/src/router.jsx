75610edf9c983495d122ba5ec1210306df69b680
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2026-02-22 02:04:09 -0700

    Updated the router for pagination

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,16 +1,26 @@
-/*
-	Router setup: if you don't have react-router-dom installed run:
-		npm install react-router-dom
-*/
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import LoginPage from "./pages/Login/Login";
+import { isAuthed } from "./api/auth";
+
+function ProtectedRoute({ children }) {
+  return isAuthed() ? children : <Navigate to="/login" replace />;
+}
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
