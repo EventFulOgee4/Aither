157ea13650c257846e59da4969a4d0a96b32880b
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2026-03-31 04:29:35 -0600

    extended the refresh token's timeouts

--- backend/aitherapist/settings.py
+++ backend/aitherapist/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 # Internationalization
